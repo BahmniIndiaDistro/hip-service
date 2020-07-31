949d267a943d5dda0cf04637ba30f48abbc1ced6
Gwan Thanakrit <thanakrit.j@thoughtworks.com>
2020-07-31 11:57:36 +0700

    HIP #41 | Gwan Cecily Zi | Fetch carecontexts for hip patient.

--- src/In.ProjectEKA.HipService/OpenMrs/OpenMrsPatientRepository.cs
+++ src/In.ProjectEKA.HipService/OpenMrs/OpenMrsPatientRepository.cs
@@ -9,10 +9,12 @@ namespace In.ProjectEKA.HipService.OpenMrs
     public class OpenMrsPatientRepository : IPatientRepository
     {
         private readonly IPatientDal _patientDal;
+        private readonly ICareContextRepository _careContextRepository;
 
-        public OpenMrsPatientRepository(IPatientDal patientDal)
+        public OpenMrsPatientRepository(IPatientDal patientDal, ICareContextRepository careContextRepository)
         {
             _patientDal = patientDal;
+            _careContextRepository = careContextRepository;
         }
 
         public Option<Patient> PatientWith(string referenceNumber)
@@ -22,9 +24,11 @@ namespace In.ProjectEKA.HipService.OpenMrs
 
         public async Task<Option<Patient>> PatientWithAsync(string referenceNumber)
         {
-            var result = await _patientDal.LoadPatientAsync(referenceNumber);
+            var fhirPatient = await _patientDal.LoadPatientAsync(referenceNumber);
+            var hipPatient = fhirPatient.ToHipPatient(fhirPatient.Name[0].Text);
+            hipPatient.CareContexts = await _careContextRepository.GetCareContexts(referenceNumber);
 
-            return Option.Some(result.ToHipPatient(result.Name[0].Text));
+            return Option.Some(hipPatient);
         }
     }
 }
--- test/In.ProjectEKA.HipServiceTest/Link/PatientRepositoryTest.cs
+++ test/In.ProjectEKA.HipServiceTest/Link/PatientRepositoryTest.cs
@@ -8,6 +8,8 @@ using OpenMrsPatientName = Hl7.Fhir.Model.HumanName;
 using OpenMrsGender = Hl7.Fhir.Model.AdministrativeGender;
 using FluentAssertions;
 using In.ProjectEKA.HipLibrary.Patient.Model;
+using In.ProjectEKA.HipLibrary.Patient;
+using System.Linq;
 
 namespace In.ProjectEKA.HipServiceTest.Link
 {
@@ -15,9 +17,15 @@ namespace In.ProjectEKA.HipServiceTest.Link
     public class PatientRepositoryTest
     {
         private Mock<IPatientDal> patientDal = new Mock<IPatientDal>();
+        private Mock<ICareContextRepository> careContextRepository = new Mock<ICareContextRepository>();
 
         public PatientRepositoryTest()
         {
+            var careContextRepresentations = new[]
+            {
+                new CareContextRepresentation("testReferrenceNumber1", "display1"),
+                new CareContextRepresentation("testReferenceNumber2", "display2")
+            };
             patientDal.Setup(e => e.LoadPatientAsync(It.IsAny<string>()))
                 .Returns(Task.FromResult(
                     new OpenMrsPatient() {
@@ -26,20 +34,27 @@ namespace In.ProjectEKA.HipServiceTest.Link
                         BirthDate = "1981"
                     }
                 ));
+            careContextRepository.Setup(e => e.GetCareContexts(It.IsAny<string>())).Returns(Task.FromResult(
+                new List<CareContextRepresentation>(careContextRepresentations).AsEnumerable()
+            ));
         }
 
         [Fact]
         private async void PatientRepositoryPatientWith_ReturnHIPPatient()
         {
             var patientId = "someid";
-            var repo = new OpenMrsPatientRepository(patientDal.Object);
+            var repo = new OpenMrsPatientRepository(patientDal.Object, careContextRepository.Object);
 
             var patient = await repo.PatientWithAsync(patientId);
 
             patientDal.Verify( x => x.LoadPatientAsync(patientId), Times.Once);
-            patient.ValueOr(new Patient()).Name.Should().Be("test");
-            patient.ValueOr(new Patient()).Gender.Should().Be(Gender.F);
-            patient.ValueOr(new Patient()).YearOfBirth.Should().Be(1981);
+            careContextRepository.Verify(x => x.GetCareContexts(patientId), Times.Once);
+            var patientValue = patient.ValueOr(new Patient());
+            patientValue.Name.Should().Be("test");
+            patientValue.Gender.Should().Be(Gender.F);
+            patientValue.YearOfBirth.Should().Be(1981);
+            patientValue.CareContexts.First().ReferenceNumber.Should().Be("testReferrenceNumber1");
+            patientValue.CareContexts.Count().Should().Be(2);
         }
     }
 }
