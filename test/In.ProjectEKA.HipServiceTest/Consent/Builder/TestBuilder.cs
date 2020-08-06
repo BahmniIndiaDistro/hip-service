965919cd0df098fb232923349f1fac4fadffb7d1
David Viktor Koltai <davidviktor.koltai@thoughtworks.com>
2020-08-06 12:52:09 +0700

    HIP#32 | David | Remove deprecated endpoint and tests

--- test/In.ProjectEKA.HipServiceTest/Consent/Builder/TestBuilder.cs
+++ test/In.ProjectEKA.HipServiceTest/Consent/Builder/TestBuilder.cs
@@ -3,20 +3,12 @@ namespace In.ProjectEKA.HipServiceTest.Consent.Builder
     using Bogus;
     using Common.Builder;
     using HipService.Common.Model;
-    using HipService.Consent;
     using HipService.Consent.Model;
 
     public class TestBuilder
     {
         private static Faker faker = new Faker();
         internal static Faker Faker() => faker ??= new Faker();
-
-        internal static ConsentArtefactRequest ConsentArtefactRequest()
-        {
-            return new ConsentArtefactRequest(faker.Random.Hash(),
-                ConsentArtefact().Generate().Build(),
-                ConsentStatus.GRANTED, faker.Random.Hash());
-        }
 
         internal static Notification Notification()
         {
